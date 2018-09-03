a0f1024a157ab70a21116f4375025ecf9d2701c0
hyyqc <junjie_hyyqc@163.com>
2018-09-03 14:39:38 +0800

    fix

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 export default class TodoList extends Component {
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <Fragment>
+        <input type="text"/><button>提交</button>
+        <ul>
+          <li>学英语</li>
+          <li>学英语</li>
+        </ul>
+      </Fragment>
     );
   }
 }
