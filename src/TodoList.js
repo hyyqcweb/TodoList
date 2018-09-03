68647a22077dd29a1510ae8e92be7b9aa9705166
hyyqc <junjie_hyyqc@163.com>
2018-09-03 14:58:40 +0800

    todolist fix

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,32 @@
 import React, { Component, Fragment } from 'react';
 
 export default class TodoList extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      inputValue:e.target.value
+    })
+  };
+
   render() {
     return (
       <Fragment>
-        <input type="text"/><button>提交</button>
+       <div>
+        <input 
+          type="text" 
+          value={this.state.inputValue} 
+          onChange={this.handleInputChange}
+        />
+        <button>提交</button>
+       </div>
         <ul>
           <li>学英语</li>
           <li>学英语</li>
