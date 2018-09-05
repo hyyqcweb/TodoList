f50155a8804a4a6214df559890676074c0650e21
hyyqc <junjie_hyyqc@163.com>
2018-09-05 16:02:05 +0800

    add transition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React , { Component, Fragment} from 'react';
+import React , { Component, Fragment } from 'react';
 import './App.css';
+import { CSSTransition } from 'react-transition-group';
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			show: true
-		}
-		this.handleToggole = this.handleToggole.bind(this)
-	}
-	handleToggole() {
-		this.setState(() => ({
-			show: this.state.show ? false : true
-		}))
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: true
+        }
+        this.handleToggole = this.handleToggole.bind(this)
+    }
+    handleToggole() {
+        this.setState(() => ({
+            show: this.state.show ? false : true
+        }))
+    }
     render() {
         return (
             <Fragment>
-            	<div className={this.state.show ? 'show' : 'hide'}>hello</div>
+	            <CSSTransition
+					in={this.state.show}
+					timeout={1000}
+					classNames='fade'
+					unmountOnExit
+					onEntered={(el) => {
+						return el.style.color='blue';
+					}}
+					appear={true}
+	            >
+	            	<div>hello</div>
+	            </CSSTransition>
             	<button onClick={this.handleToggole}>toggole</button>
 			</Fragment>
         )
