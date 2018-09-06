065f29232b27e3ad858f2d6e29c6a26fcf1aa303
hyyqc <junjie_hyyqc@163.com>
2018-09-06 15:59:13 +0800

    拆分常量组件

--- src/refactor/TodoList.js
+++ src/refactor/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
 import store from './store';
+import {CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionTypes'
 
 export default class TodoList extends Component {
 	constructor (props) {
@@ -45,7 +46,7 @@ export default class TodoList extends Component {
     }
     handleInputChange(e){
 		const action = {
-			type: 'change_input_value',
+			type: CHANGE_INPUT_VALUE,
 			value: e.target.value
 		}
 		store.dispatch(action);
@@ -55,13 +56,13 @@ export default class TodoList extends Component {
     }
     handleBtnClick() {
 		const action = {
-    		type: 'add_todo_item'
+    		type: ADD_TODO_ITEM
     	}
     	store.dispatch(action);
     }
     handleItemDelete(index) {
 		const action = {
-			type:'delete_todo_item',
+			type:DELETE_TODO_ITEM,
 			index
 		} 
 		store.dispatch(action);  	
