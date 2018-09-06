81d6618a6f6cec0a8b1f5703a1452a4241414f8f
hyyqc <junjie_hyyqc@163.com>
2018-09-06 14:24:44 +0800

    add store

--- src/refactor/TodoList.js
+++ src/refactor/TodoList.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
-
+import store from './store';
 
 export default class TodoList extends Component {
+	constructor (props) {
+		super(props);
+		this.state = store.getState();
+	}
     render() {
         return (
             <div style={{
                 margin: '5% 0 0 5%'
             }}>
-				<Input placeholder="todo info" style={{
+				<Input  placeholder="todo info" style={{
                 width: 300,
                 marginRight: 10
             }}/>
@@ -28,7 +24,7 @@ export default class TodoList extends Component {
 		                width: 300
 		            }}
 		            bordered
-		            dataSource={data}
+		            dataSource={this.state.list}
 		            renderItem={item => (<List.Item>{item}</List.Item>)}
 	            />
 			</div>
