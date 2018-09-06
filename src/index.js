02e7350ee1a3aee685dd4da947abf23376919e01
hyyqc <junjie_hyyqc@163.com>
2018-09-06 10:54:14 +0800

    todoList 引入antd,redux管理

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import App2 from './App2';
+import TodoList from './refactor/TodoList'
 
-ReactDOM.render(<App2 />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
