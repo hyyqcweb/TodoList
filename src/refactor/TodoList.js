68ec8df2112c9e9b4e05b948f345b06b3944b146
hyyqc <junjie_hyyqc@163.com>
2018-09-06 13:54:49 +0800

    引入antd 重构代码

--- src/refactor/TodoList.js
+++ src/refactor/TodoList.js
@@ -1,11 +1,37 @@
-import React,{Component,Fragment} from 'react';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
+
 
 export default class TodoList extends Component {
-	render() {
-		return (
-			<div>
-				123
+    render() {
+        return (
+            <div style={{
+                margin: '5% 0 0 5%'
+            }}>
+				<Input placeholder="todo info" style={{
+                width: 300,
+                marginRight: 10
+            }}/>
+				<Button type="primary">提交</Button>
+				<List
+		            style={{
+		                marginTop: 10,
+		                width: 300
+		            }}
+		            bordered
+		            dataSource={data}
+		            renderItem={item => (<List.Item>{item}</List.Item>)}
+	            />
 			</div>
-		)
-	}
+        )
+    }
 }
