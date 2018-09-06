fe948195b795c494cb836c24b5043f7656de0406
hyyqc <junjie_hyyqc@163.com>
2018-09-06 15:19:28 +0800

    store

--- src/refactor/TodoList.js
+++ src/refactor/TodoList.js
@@ -7,6 +7,10 @@ export default class TodoList extends Component {
 	constructor (props) {
 		super(props);
 		this.state = store.getState();
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleStoreChange = this.handleStoreChange.bind(this);
+		this.handleBtnClick = this.handleBtnClick.bind(this);
+		store.subscribe(this.handleStoreChange); // subscribe 订阅
 	}
     render() {
         return (
@@ -14,10 +18,13 @@ export default class TodoList extends Component {
                 margin: '5% 0 0 5%'
             }}>
 				<Input  placeholder="todo info" style={{
-                width: 300,
-                marginRight: 10
-            }}/>
-				<Button type="primary">提交</Button>
+	                width: 300,
+	                marginRight: 10
+	            }}
+	            value={this.state.inputValue}
+	            onChange= {this.handleInputChange}
+	            />
+				<Button type="primary" onClick={this.handleBtnClick}>提交</Button>
 				<List
 		            style={{
 		                marginTop: 10,
@@ -30,4 +37,21 @@ export default class TodoList extends Component {
 			</div>
         )
     }
+    handleInputChange(e){
+		const action = {
+			type: 'change_input_value',
+			value: e.target.value
+		}
+		store.dispatch(action);
+    }
+    handleStoreChange() {
+    	this.setState(store.getState())
+    }
+    handleBtnClick() {
+    	const action = {
+    		type: 'add_todo_item'
+    	}
+    	store.dispatch(action);
+    }
+
 }
