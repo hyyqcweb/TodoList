d31514e5e666b17851cdf0335e1dccf821053523
hyyqc <junjie_hyyqc@163.com>
2018-09-10 10:23:00 +0800

    react-redux

--- src/secondRefactor/TodoList.js
+++ src/secondRefactor/TodoList.js
@@ -1,36 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
-class TodoList extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
-
-	
-
-	handleClick = () => {
-
-	};
-
-    render() {
-        return (
+// TodoList 是UI组件
+const TodoList = ({inputValue, handleInputChange, handleClick, handleDelete, list}) => {
+	return (
             <div>
 				<div>
-					<input type="text" value={this.props.inputValue} onChange={this.props.handleInputChange}/>
-					<button onClick={this.handleClick}>提交</button>
+					<input type="text" value={inputValue} onChange={handleInputChange}/>
+					<button onClick={handleClick}>提交</button>
 				</div>
 				<ul>
-					<li>123</li>
+					{
+						list.map((item,index) => {
+							return <li onClick={ () => handleDelete(index)} key={index}>{item}</li>
+						})
+					}
 				</ul>
 			</div>
         )
-    }
 }
 // 遍历store中的数据转换为props
 const mapStateToProps = (state) => {
     return {
-        inputValue: state.inputValue
+        inputValue: state.inputValue,
+        list: state.list
     }
 }
 // dispatch => store.dispatch
@@ -40,10 +33,23 @@ const mapDispatchToProps = (dispatch) => {
 			const action = {
 				type: 'change_input_value',
 				value: e.target.value
-			}
+			};
+			dispatch(action);
+		},
+		handleClick() {
+			const action = {
+				type: 'add_item'
+			};
+			dispatch(action);
+		},
+		handleDelete(index) {
+			const action = {
+				type: 'remove_item',
+				index
+			};
 			dispatch(action);
 		}
 	}
 }
-
+// connect 返回的结果是 容器组件
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
